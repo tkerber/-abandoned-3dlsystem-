41450e9088b41555086b3f6945c7bddf0618361b
Thomas Kerber <t.kerber@online.de>
2013-11-14 23:46:36 +0000

    Added cylinders.

---
+++ Cylinder.hs
@@ -0,0 +1,50 @@
+module Cylinder(
+  drawCylinder
+) where
+-- I am not working with OpenGL's vector type for as much of this program as
+-- I can, as my grasp of monads is shaky at best, and that of OpenGL even
+-- worse.
+import Vector
+import Graphics.Rendering.OpenGL
+
+
+-- First the normal to the quad, then its 4 verticies.
+type Quad = (Vector GLfloat, Vector GLfloat, Vector GLfloat, Vector GLfloat,
+  Vector GLfloat)
+
+
+-- start point, end point, radius, sides
+cylinder :: Vector GLfloat -> Vector GLfloat -> GLfloat -> Int -> [Quad]
+cylinder s e r' n = [(
+    norm i,
+    r i /+ s,
+    r i /+ e,
+    r (i + 1) /+ e,
+    r (i + 1) /+ s
+  ) | i <- [0..n - 1]]
+  where
+    d = e /- s
+    r 0 = orth /* (r' / len orth)
+    r i = rot d (2 * pi * (fromIntegral i) / n') (r 0)
+    norm i = rot d (2 * pi * ((fromIntegral i) + 0.5) / n') (r 0)
+    orth = orthogonal d
+    n' = fromIntegral n
+
+drawQuads :: [Quad] -> IO ()
+drawQuads q = do
+  mapM (\(n, v1, v2, v3, v4) -> do
+    renderPrimitive Quads $ do
+      normal (toNorm n)
+      vertex (toVertex v1)
+      vertex (toVertex v2)
+      vertex (toVertex v3)
+      vertex (toVertex v4)) q
+  return ()
+
+drawCylinder :: Vector GLfloat -> Vector GLfloat -> GLfloat -> Int -> IO ()
+drawCylinder s e r n = drawQuads $ cylinder s e r n
+
+-- a = Vector 1 2 3 `dot` Vector 3 2 1
+--cylinder :: Vector GLfloat -> Vector GLfloat -> GLfloat -> Int ->
+--    [[Vector GLfloat]]
+--cylinder from to width i = undefined
---
+++ Vector.hs
@@ -0,0 +1,108 @@
+module Vector(
+  Vector(Vector),
+  (/+),
+  (/-),
+  (/*),
+  (//),
+  neg,
+  dot,
+  cross,
+  norm,
+  len,
+  orthogonal,
+  rot,
+  toNorm,
+  toVertex
+) where
+
+import Graphics.Rendering.OpenGL
+
+-- As I'm only dealing with 3D vectors here, I am being a bit lazy and not
+-- generalizing.
+-- I realize there are types for vectors and classes for them which I could
+-- use, but I'd have to get aquianted with them first and it seems too much
+-- of a hassle.
+data Vector a = Vector a a a deriving(Eq, Ord)
+
+x :: Vector a -> a
+x (Vector a _ _) = a
+y :: Vector a -> a
+y (Vector _ a _) = a
+z :: Vector a -> a
+z (Vector _ _ a) = a
+
+instance Show a => Show (Vector a) where
+  show (Vector a b c) = "(" ++ show a ++ "," ++ show b ++ "," ++ show c ++ ")"
+
+(/+) :: Num a => Vector a -> Vector a -> Vector a
+(Vector a b c) /+ (Vector a' b' c') = Vector (a + a') (b + b') (c + c')
+
+(/-) :: Num a => Vector a -> Vector a -> Vector a
+v1 /- v2 = v1 /+ neg v2
+
+(/*) :: Num a => Vector a -> a -> Vector a
+(Vector a b c) /* x = Vector (a * x) (b * x) (c * x)
+
+(//) :: Fractional a => Vector a -> a -> Vector a
+v // x = v /* (1 / x)
+
+neg :: Num a => Vector a -> Vector a
+neg (Vector a b c) = Vector (-a) (-b) (-c)
+
+dot :: Num a => Vector a -> Vector a -> a
+(Vector a b c) `dot` (Vector a' b' c') = a * a' + b * b' + c * c'
+
+cross :: Num a => Vector a -> Vector a -> Vector a
+(Vector a b c) `cross` (Vector a' b' c') = Vector
+  (b * c' - c * b')
+  (c * a' - a * c')
+  (a * b' - b * a')
+
+norm :: Floating a => Vector a -> Vector a
+norm v = v // len v
+
+len :: Floating a => Vector a -> a
+len v = sqrt $ v `dot` v
+
+-- Finds a vector orthogonal to the given vector.
+orthogonal :: (Num a, Eq a) => Vector a -> Vector a
+orthogonal (Vector a 0 0) = Vector 0 1 0
+orthogonal v = v `cross` Vector 1 0 0
+
+-- A bit ugly, but it works. And no, I'm not making a matrix type just for a
+-- few rotations.
+-- axis -> angle -> vector -> new vector
+rot :: Floating a => Vector a -> a -> Vector a -> Vector a
+rot u a v = Vector
+  (
+    ((1 - cos') + ux * ux * cos') * vx +
+    (ux * uy * cos' - uz * sin') * vy +
+    (ux * uz * cos' + uy * sin') * vz
+  )
+  (
+    (uy * ux * cos' + uz * sin') * vx +
+    ((1 - cos') + uy * uy * cos') * vy +
+    (uy * uz * cos' - ux * sin') * vz
+  )
+  (
+    (uz * ux * cos' - uy * sin') * vx +
+    (uz * uy * cos' + ux * sin') * vy +
+    ((1 - cos') + uz * uz * cos') * vz
+  )
+  where
+    u' = norm u
+    ux = x u
+    uy = y u
+    uz = z u
+    vx = x v
+    vy = y v
+    vz = z v
+    sin' = sin a
+    cos' = 1 - cos a
+
+toNorm :: Vector a -> Normal3 a
+toNorm (Vector a b c) = Normal3 a b c
+
+toVertex :: Vector a -> Vertex3 a
+toVertex (Vector a b c) = Vertex3 a b c
+
